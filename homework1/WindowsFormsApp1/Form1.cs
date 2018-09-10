f3039d3811713875482537b3d07b0efc6d6bc276
1025058301 <1025058301@qq.com>
2018-09-11 00:11:52 +0800

    the first homework has some bug and check this

---
+++ homework1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string s;
+        string s2;
+        double a;
+        double a2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            s = textBox1.Text;
+           
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            s2 = textBox2.Text;
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(s);
+            a2 = double.Parse(s2);
+            double b = a * a2;
+            label1.Text = "积是"+b;
+        }
+    }
+}
---
+++ homework1/homework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please input an int");
+            string a = System.Console.ReadLine();
+            int a1 = Int32.Parse(a);
+            System.Console.WriteLine("second int");
+            string b = System.Console.ReadLine();
+            int b1= Int32.Parse(b);
+            int c = a1 * b1;
+            System.Console.WriteLine("result is :" + c);
+        }
+    }
+}
