db85cecd0e0841a5deaefecdb253d725bb69d5dd
1025058301 <1025058301@qq.com>
2018-10-18 21:12:55 +0800

    fifth homework

---
+++ homework5/fifthhomework1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work2
+{
+    class Order
+    {
+        public int ordernumber;
+        public string ordername;
+        public string orderclientname;
+        public int ordermoney;
+        public Order(int a, string b, string c,int d)
+        {
+            ordernumber = a;
+            ordername = b;
+            orderclientname = c;
+            ordermoney = d;
+
+        }
+
+    }
+    class OrderService
+    {
+        
+        List<Order> list = new List<Order>();
+        public void Addorder(int a1, string b2, string c2,int d2)
+        {
+            Order neworder = new Order(a1, b2, c2,d2);
+            list.Add(neworder);
+
+        }
+        public void Deleteorder(int a)
+        {
+            foreach (Order i in list)
+            {
+                if (i.ordernumber == a)
+                {
+                    list.Remove(i);
+                }
+
+            }
+        }
+        public void Enquirorderfromnumber(int a)
+        {
+            foreach (Order i in list)
+            {
+                if (i.ordernumber == a)
+                {
+                    Console.WriteLine("订单名称：", i.ordername, "客户名称：", i.orderclientname);
+                }
+
+            }
+        }
+        public void Enquiroderfromordername(string a)
+        {
+            int time = 0;
+            foreach (Order i in list)
+            {
+                if (i.ordername == a)
+                {
+                    Console.WriteLine("订单号：", i.ordernumber, "客户名称：", i.orderclientname);
+                    time++;
+                }
+            }
+            if (time == 0)
+            {
+                Console.WriteLine("SORRY,NO THIS ORDER");
+            }
+        }
+        public void EnquiroEnquiroderfromordernameLQ(string a)
+        {
+            int time = 0;
+            var m = from n in list
+                    where n.ordername == a
+                    select n;
+            foreach(var n in m)
+            {
+                Console.WriteLine(n.ordernumber+"  "+ n.orderclientname);
+                time++;
+
+            }
+            if (time == 0)
+            {
+                Console.WriteLine("Sorrry,No this order");
+            }
+
+        }
+        public void Enquiroderfromclientname(string a)
+        {
+            foreach (Order i in list)
+            {
+                if (i.orderclientname == a)
+                {
+                    Console.WriteLine("订单号：", i.ordernumber, "订单名称：", i.ordername);
+                }
+            }
+        }
+        public void EnquiroEnquiroderfromclientordernameLQ(string a)
+        {
+            int time = 0;
+            var m = from n in list
+                    where n.orderclientname == a
+                    select n;
+            foreach (var n in m)
+            {
+                Console.WriteLine(n.ordernumber + "  " + n.ordername);
+                time++;
+
+            }
+            if (time == 0)
+            {
+                Console.WriteLine("Sorrry,No this order");
+            }
+
+        }
+        public void Enquirordermoney()
+        {
+            int time = 0;
+             var m = from n in list
+                        where n.ordermoney >= 10000
+                        select n;
+            foreach(var n in m)
+            {
+                Console.WriteLine(n.ordernumber + "  " + n.ordername + "  " + n.orderclientname);
+                time++;
+            }
+            if (time == 0)
+            {
+                Console.WriteLine("no one order's money > 10000");
+            }
+        }
+        public void Reviseorder()
+        {
+            int time = 0;
+            Console.WriteLine("please input the order number which you want to revise");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("do you want to revise the ordername? if yes please input the new name");
+            string b = Console.ReadLine();
+            Console.WriteLine("do you want to revise the clientname? if yes please input the new name");
+            string c = Console.ReadLine();
+            foreach (Order i in list)
+            {
+                if (i.ordernumber == a)
+                {
+                    i.ordername = b;
+                    i.orderclientname = c;
+                    time++;
+                }
+            }
+            if (time == 0)
+            {
+                Console.WriteLine("SORRY,NO THIS ORDER");
+            }
+        }
+        public static void Main()
+        {
+            OrderService order = new OrderService();
+            order.Addorder(1, "苹果", "lcy",10000);
+            Order a = order.list[0];
+            order.EnquiroEnquiroderfromordernameLQ("苹果");
+
+        }
+
+    }
+}
---
+++ homework5/painttree/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace painttree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI/2);
+        }
+        private Graphics graphics;
+        double th1 ;
+        double th2 ;
+        double per1;
+        double per2;
+        string k;
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+          
+            if (k == "Blue")
+            {
+                graphics.DrawLine(
+
+      Pens.Blue,
+      (int)x0, (int)y0, (int)x1, (int)y1);
+            }
+            if (k == "Red")
+            {
+                graphics.DrawLine(
+
+     Pens.Red,
+     (int)x0, (int)y0, (int)x1, (int)y1);
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string s = textBox2.Text;
+            double s1 = double.Parse(s);
+            th1 = s1 * Math.PI / 180;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            double s2 = double.Parse(s);
+            th2 = s2 * Math.PI / 180;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string s = textBox3.Text;
+            per1 = double.Parse(s);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            string s = textBox4.Text;
+            per2 = double.Parse(s);
+        }
+
+        
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            k = textBox5.Text;
+
+        }
+    }
+}
